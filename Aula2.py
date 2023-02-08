ba8e84fee0a40b84d3d9cd4920952f89e884921b
AlexisCordeiro <121447713+AlexisCordeiro@users.noreply.github.com>
2023-02-08 23:36:54 +0000

    Segundo código Git

---
+++ Aula2.py
@@ -0,0 +1,10 @@
+nome = str(input("Escreva seu nome: "))
+rua = str(input("Diga o nome da sua rua: "))
+cep = float(input("Digite seu CEP: "))
+tel = float(input("Digite seu telefone: "))
+
+print(nome)
+print(rua)
+print(cep)
+print(tel)
+
